1a57708a22db71a21a915ede270f1fa3c83bfe9d
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-07-18 14:56:55 +0530

    Patient login form is done with appwrite

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import PatientForm from '@/components/forms/PatientForm'
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='flex h-screen max-h-screen'>
+
+      {/* todo: OTP varification | Passkey model */}
+
+      <section className='remove-scrollbar container my-auto'>
+        <div className='sub-container max-w-[496px]'>
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="Carepulse Logo"
+            className='mb-12 h-10 w-fit'
+          />
+
+          <PatientForm />
+
+          <div className='text-14-regular mt-20 flex justify-between'>
+            <p className='justify-end text-dark-600 xl:text-left'>&copy; {new Date().getFullYear()} CarePulse</p>
+            <Link href="/?admin=true" className='text-gray-500'>Admin</Link>
+          </div>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt='Patient'
+        className='side-img max-w-[50%]'
+      />
+    </div>
   )
 }
 
