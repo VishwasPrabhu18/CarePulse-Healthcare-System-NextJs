da1df12a4f27e51fd893da8e7883d68db4958097
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-07-20 10:44:36 +0530

    update appointment action is done

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,8 +1,10 @@
 "use server";
 
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { APPOINTMENT_COLLECTION_ID, BUCKET_ID, DATABASE_ID, databases, ENDPOINT, PATIENT_COLLECTION_ID, storage } from "../appwrite.config";
 import { parseStringify } from "../utils";
+import { Appointment } from "@/types/appwrite.types";
+import { revalidatePath } from "next/cache";
 
 export const createAppointment = async (appointmentData: CreateAppointmentParams) => {
   try {
@@ -20,16 +22,78 @@ export const createAppointment = async (appointmentData: CreateAppointmentParams
   }
 }
 
-export const getAppointments = async (appointmentId: string) => { 
+export const getAppointments = async (appointmentId: string) => {
   try {
     const appointment = await databases.getDocument(
       DATABASE_ID!,
       APPOINTMENT_COLLECTION_ID!,
       appointmentId
     );
-    
+
     return parseStringify(appointment);
   } catch (error) {
     console.log("Error occurred while fetching appointments", error);
   }
 };
+
+export const getRecentAppointmentsList = async () => {
+  try {
+    const appointments = await databases.listDocuments(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      [
+        Query.orderDesc("$createdAt"),
+      ]
+    );
+
+    const initialCount = {
+      scheduledCount: 0,
+      pendingCount: 0,
+      cancelledCount: 0,
+    };
+
+    const counts = (appointments.documents as Appointment[]).reduce((acc, appointment) => {
+      if (appointment.status === "scheduled") {
+        acc.scheduledCount += 1;
+      } else if (appointment.status === "pending") {
+        acc.pendingCount += 1;
+      } else if (appointment.status === "cancelled") {
+        acc.cancelledCount += 1;
+      }
+      return acc;
+    }, initialCount);
+
+    const data = {
+      totalCount: appointments.total,
+      ...counts,
+      documents: appointments.documents,
+    };
+
+    return parseStringify(data);
+  } catch (error) {
+    console.log("Error occurred while fetching recent appointments", error);
+  }
+}
+
+export const updateAppointment = async ({appointmentId, userId, appointment, type}: UpdateAppointmentParams) => {
+  try {
+    const updatedAppointment = await databases.updateDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId,
+      appointment
+    );
+
+    if (!updatedAppointment) {
+      throw new Error("Failed to update appointment");
+    }
+
+    // TODO: SMS Notification
+
+    revalidatePath("/admin");
+    
+    return parseStringify(updatedAppointment);
+  } catch (error) {
+    console.log("Error occurred while updating an appointment", error);
+  }
+}
